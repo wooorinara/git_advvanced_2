86c3ac17016474979e6ac43aa7603f4b50980954
wooorinara <jacklwj10@gmail.com>
2024-05-02 06:53:56 +0000

    hotfix on main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def even_list(int_list: List[int]) -> List[int]:
     Returns:
         A list of even integers.
     """
-    # TODO: Implement even_list
-    pass
+    even_list = [num for num in int_list if num % 2 == 0]
+    return even_list
 
 # Sum of squares of even numbers
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
@@ -29,9 +29,9 @@ def main():
     # Example list
     int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    # Wrongly written code
-    output = sum_of_squares_of_even(int_list)
-    print(output)
+    # Fixed wrongly written code
+    output = even_list(int_list)
+    output = sum_of_squares_of_even(output)
 
 # Boilerplate code
 if __name__ == "__main__":
